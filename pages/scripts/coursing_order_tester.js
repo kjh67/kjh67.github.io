d4b42bd27fe6af91cac31868a5876011d2773e80
K Hawkins <kjh67@cantab.ac.uk>
2025-02-22 22:32:57 +0000

    Improved coursing order trainer to simulate people going wrong.

--- pages/scripts/coursing_order_tester.js
+++ pages/scripts/coursing_order_tester.js
@@ -1,7 +1,10 @@
+// Component references
 const call_display = document.getElementById("call_display");
 const co_display = document.getElementById("co_display");
 const input = document.getElementById("input");
 const submit = document.getElementById("submit");
+const feedback = document.getElementById("feedback");
+
 const call_arrs = {
     "H":[0,2,3,1,4],
     "W":[1,2,0,3,4],
@@ -12,13 +15,10 @@ const call_arrs = {
     "sM":[0,1,4,3,2],
 };
 let calls = Object.keys(call_arrs);
-
-let co_hidden = false;
-let co = "53246";
-
 let call = ""
+let co = "53246";
+let co_hidden = false;
 
-// After previous call has been handled, move on to the next one
 function nextCall() {
     call = calls[Math.floor(Math.random()*calls.length)];
     call_display.textContent = call;
@@ -35,6 +35,25 @@ function transposeCO() {
     return new_co;
 }
 
+function conductingRequired() {
+    // Start by presenting who goes wrong, what you're doing
+    // After tried once, option to just close or submit again
+    let plain_co = "8753246";
+    wrongun = Math.floor(Math.random()*6)+2;
+    your_pb = Math.floor(Math.random()*7)+2;
+    full_co = "87"+co;
+    offset = full_co.indexOf(wrongun);
+    their_pb = plain_co[(plain_co.indexOf(your_pb)+offset)%plain_co.length];
+    user_answer = prompt("Oh no! The "+wrongun+" went wrong!!! You are the tenor ringing "
+        +your_pb+"th place bell. What place bell should the "+wrongun+" be?");
+    if (user_answer === their_pb) {
+        alert("Correct!");
+    } else {
+        alert("Incorrect. The coursing order is "+full_co+", so if you are "+your_pb
+            +"th place bell, the "+wrongun+" would be "+their_pb+"th place bell.");
+    }
+}
+
 function checkAndUpdate() {
     let user_answer = input.value;
     let correct = transposeCO();
@@ -42,15 +61,21 @@ function checkAndUpdate() {
         // Hide coursing order
         co_display.innerHTML = "<a>Show</a>";
         co_display.onclick = showCO;
-        alert("correct!");
+        feedback.textContent = "Correct";
         // Clear input box
         input.value = "";
         // Update to new CO
         co = correct;
         // Generate next call
         nextCall();
+
+        // Chance for someone to need correcting
+        if (Math.random() < 0.5) {
+            conductingRequired();
+        }
+
     } else {
-        alert("incorrect!");
+        feedback.textContent = "Incorrect! Try Again";
     }
 }
 
